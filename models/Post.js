981ab2d70ba9174dba8f2588317ff392523af38b
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-19 08:42:11 -0400

    Post Model defined

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,18 @@
+const { db, Sequelize } = require("../db/connection");
+const { Model, DataTypes } = require("sequelize");
 
-let Post;
+class Post extends Model {}
 
+Post.init(
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+  },
+  {
+    sequelize: db,
+    modelName: "Post",
+  }
+);
 
 module.exports = Post;
