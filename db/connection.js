cd7e2512153552e1a17a6e9874a055ff55530721
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-18 22:36:59 -0400

    sequelize connection created

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
+const path = require("path")
+const {Sequelize}= require("sequelize")
 
+const db= new Sequelize({
+ dialect:"sqlite",
+ storage:path.join(__dirname,"db.sqlite")
+})
 
 
 
 
 module.exports = {
     db,
-    
+    Sequelize
 }
